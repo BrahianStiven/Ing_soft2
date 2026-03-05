388421dbde60b577a8b6ea28882bb6a3ac510208
BrahianStiven <becheverryq@unal.edu.co>
2026-03-04 22:01:22 -0500

    chore: django endpoint modify

--- Django/mi_api/api/views.py
+++ Django/mi_api/api/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Producto
-from .serializars import ProductoSerializer
+from .serializers import ProductoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .permissions import ValidarTokenPersonalizado
 from django.http import JsonResponse
-# Create your views here.
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+    permission_classes = [ValidarTokenPersonalizado]
 
 @api_view(['GET'])
 def health(request):
